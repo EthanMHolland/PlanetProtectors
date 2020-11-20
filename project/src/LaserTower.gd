1e990074c595488343c8f2c6ef114dc8f072d144
Logansummers18 <logansummers27@att.net>
2020-11-19 22:38:12 -0500

    Add Tower Health Bar

--- project/src/LaserTower.gd
+++ project/src/LaserTower.gd
@@ -4,6 +4,10 @@ signal tower_destroyed
 
 const LASER = preload("Laser.tscn")
 
+var _bar_red = preload("res://assets/barHorizontal_red.png")
+var _bar_green = preload("res://assets/barHorizontal_green.png")
+var _bar_yellow = preload("res://assets/barHorizontal_yellow.png")
+
 var _health := 3
 var _shots_left := 15
 
@@ -13,10 +17,11 @@ func _ready():
 
 
 func _process(_delta):
+	_check_health()
 #deletes tower if health is 0 or below
-	if _health <= 0:
-		emit_signal("tower_destroyed")
-		queue_free()
+#	if _health <= 0:
+#		emit_signal("tower_destroyed")
+#		queue_free()
 
 #deletes tower if amount of shots left is 0 or below
 	if _shots_left <= 0:
@@ -43,3 +48,17 @@ func _shoot_laser():
 func _on_LaserTower_area_entered(area):
 	if area.is_in_group("Asteroid"):
 		_health -= 1
+
+
+func _check_health():
+	if _health == 3:
+		$HealthBar.value = 3
+	elif _health == 2:
+		$HealthBar.value = 2
+		$HealthBar.texture_progress = _bar_yellow
+	elif _health == 1:
+		$HealthBar.value = 1
+		$HealthBar.texture_progress = _bar_red
+	elif _health == 0:
+		$HealthBar.value == 0
+		queue_free()
