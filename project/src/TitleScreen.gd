a10ac23ba750737b51004b6469f34638d95cdfa6
Logansummers18 <logansummers27@att.net>
2020-11-22 13:02:45 -0500

    Fix Title Screen Tower

--- project/src/TitleScreen.gd
+++ project/src/TitleScreen.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+func _ready():
+	$LaserTower/HealthBar.hide()
+	$LaserTower._shots_left = 9999999999
+
 func _on_PlayButton_button_down():
 	var _ignored = get_tree().change_scene("res://src/Level.tscn")
 
