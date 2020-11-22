b0daf73537457b1686b0fc9d6f592f2db8185eab
ethanholland <ethanmhollan@gmail.com>
2020-11-22 12:30:50 -0500

    Add Info Screen

---
+++ project/src/InfoScreen.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_set_default_text()
+
+
+func _on_LaserTower_mouse_entered():
+	$InfoText.text = "Laser Tower\n\n- Cost $10 to place\n- Battery lasts for 15 shots before dying\n- Can take 3 hits from asteroids before being destroyed"
+
+
+func _set_default_text():
+	$InfoText.text = "Welcome to Planet Protectors!\nThere are asteroids heading toward Earth and we need your help to stop them!\n\n- Place Towers in the grid to fight back!\n- Catch Shooting Stars for extra money!\n- Beat Wave 15 to win!\n\nGood Luck!"
+
+
+func _on_BackButton_button_down():
+	var _ignored = get_tree().change_scene("res://src/TitleScreen.tscn")
+
+
+func _on_Asteroid_mouse_entered():
+	$InfoText.text = "Asteroid\n\n- Can take 1 hit from a Laser or Tower before being destroyed\n- Speed increases with wave count"
+	
+
+func _on_LaserTower_mouse_exited():
+	_set_default_text()
+
+
+func _on_Asteroid_mouse_exited():
+	_set_default_text()
+
+
+func _on_Cheats_mouse_entered():
+	$InfoText.text = "Cheats - Press button to activate\n\n\"9\" - Spawn Shooting Star\n\"0\" - Add $10\n\"-\" - Stop Spawning Asteroids\n\"+\" - Increase Wave"
+
+
+func _on_Cheats_mouse_exited():
+	_set_default_text()
--- project/src/TitleScreen.gd
+++ project/src/TitleScreen.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _on_PlayButton_button_down():
 	var _ignored = get_tree().change_scene("res://src/Level.tscn")
+
+
+func _on_InfoButton_button_down():
+	var _ignored = get_tree().change_scene("res://src/InfoScreen.tscn")
