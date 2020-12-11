2815ea319db4ad434df367e20a72ebaeee9999c4
ethanholland <ethanmhollan@gmail.com>
2020-12-11 18:56:04 -0500

    Added particle effects to asteroid collisions. Removed Unused healthbar assets

--- project/src/Asteroid.gd
+++ project/src/Asteroid.gd
@@ -6,9 +6,12 @@ signal hit
 var _speed : int = -50
 var _velocity := Vector2()
 var _health := 3
+var _stunned = false
 
 
 func _process(_delta):
+	if _health > 0:
+		_check_health()
 	if Input.is_action_just_pressed("increase_wave"):
 		queue_free()
 
@@ -17,11 +20,13 @@ func _physics_process(_delta):
 	var _direction := Vector2(-1, 0)
 	_velocity.x = _speed
 	_velocity = move_and_slide(_velocity, Vector2(-1, 0))
-	rotation -= 0.01
+	$Sprite.rotation -= 0.01
 
 #detects when a laser hit the asteroids and deletes the asteroid/laser and emits "destroyed"
 func _on_Hitbox_body_entered(body):
 	if body.is_in_group("Laser"):
+		$CPUParticles2D.emitting = true
+		$ParticleTimer.start()
 		emit_signal("hit")
 		_health -= 1
 		_check_health()
@@ -31,6 +36,8 @@ func _on_Hitbox_body_entered(body):
 #detects when the asteroids hits a tower and deletes the asteroid and emits "destroyed"
 func _on_Hitbox_area_entered(area):
 	if area.is_in_group("Tower"):
+		$CPUParticles2D.emitting = true
+		$ParticleTimer.start()
 		emit_signal("hit")
 		_health -= 1
 		_check_health()
@@ -44,4 +51,10 @@ func _check_health():
 		$Sprite.play("small")
 	elif _health <= 0:
 		emit_signal("destroyed")
+		$Hitbox.set_deferred("disabled", true)
+		$Sprite.hide()
+
+
+func _on_ParticleTimer_timeout():
+	if _health <= 0:
 		queue_free()
